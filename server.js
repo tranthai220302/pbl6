1d8b50419355be4e398d4c09b982b085f11131b8
Tran Quang THAI <tranthai220203@gmail.com>
2023-10-21 02:26:18 +0700

    cors1

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 const port = process.env.PORT
 //Connect database
 try {
