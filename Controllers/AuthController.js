432404d0cab533e21ed81f799d2badd7e79c8cd6
Tran Quang THAI <tranthai220203@gmail.com>
2023-10-21 09:02:15 +0700

    cir

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -13,7 +13,7 @@ export const loginController = async(req, res, next) =>{
             httpOnly: true,
             secure: true,
             sameSite: 'None',
-            domain: 'bookstore-gi4a.onrender.com',
+            domain: 'www.harumi.store',
             path: "/",
         }).status(200).send('Đăng nhập thành công!')
     } catch (error) {
