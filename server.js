d34b3b889c1228f92693660d133202cd8d697143
Tran Quang THAI <tranthai220203@gmail.com>
2023-12-31 10:37:45 +0700

    fix server

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ import routerCategory from "./Routes/CategoryRoute.js";
 import { Server } from "socket.io";
 import routerReportStore from "./Routes/ReportStoreRoute.js";
 import routerShippemt from "./Routes/ShippementRoute.js";
-import routerReportShipper from "./Routes/ReportShipperRoute.js";
 import routerFeedBack from "./Routes/FeedBackRoute.js";
 import routerReviewShipper from "./Routes/ReviewShipperRoute.js";
 import path from "path";
@@ -82,7 +81,6 @@ app.use('/api/chat', routerChat)
 app.use('/api/category', routerCategory)
 app.use('/api/report', routerReportStore)
 app.use('/api/shippemt',routerShippemt)
-app.use('/api/reportShipper',routerReportShipper)
 app.use('/api/feedBack', routerFeedBack)
 app.use('/api/reviewShipper',routerReviewShipper)
 app.use((err, req, res, next)=>{
